6727a2f90fff6b4ebf1c3a507c941d0783468214
Roupesh <Roup1708@gmail.com>
2024-05-22 01:08:20 +0400

    week4 update

---
+++ Week4/Day1/XP1/XP1.sql
@@ -0,0 +1,47 @@
+-- CREATE TABLE items (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(50),
+--     price INT
+-- );
+
+-- CREATE TABLE customers (
+--     id SERIAL PRIMARY KEY,
+--     firstname VARCHAR(50),
+--     lastname VARCHAR(50)
+-- );
+
+-- INSERT INTO items VALUES
+-- (1, 'Small Desk', 100),
+-- (2, 'Large Desk', 300),
+-- (3, 'Fan', 80);
+
+-- INSERT INTO customers VALUES
+-- (1, 'Greg', 'Jones'),
+-- (2 'Sandra', 'Jones'),
+-- (3 'Scott', 'Scott'),
+-- (4 'Trevor', 'Green'),
+-- (5 'Melanie', 'Johnson');
+
+-- SELECT * FROM items;
+
+-- SELECT * FROM items WHERE price > 80;
+
+-- SELECT * FROM items WHERE price <= 300;
+
+-- SELECT * FROM customers;
+
+-- SELECT * FROM customers WHERE lastname = 'Smith';
+
+-- This query will return no results
+
+-- SELECT * FROM customers WHERE lastname = 'Jones';
+
+-- SELECT * FROM customers WHERE firstname <> 'Scott';
+
+
+
+
+
+
+
+
